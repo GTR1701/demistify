ca371b54040cbe3b2e1d97fd6dfc0e5c2fffe2bf
GTR1701 <dawidkowal04@gmail.com>
2023-04-04 19:55:36 +0200

    initial config and installations

---
+++ lib/firebase.ts
@@ -0,0 +1,24 @@
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { GoogleAuthProvider, getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAr6nfXDuIj5467zchVtZ1dYZW-mRBnWmk",
+  authDomain: "codetastic-4a686.firebaseapp.com",
+  projectId: "codetastic-4a686",
+  storageBucket: "codetastic-4a686.appspot.com",
+  messagingSenderId: "833741969388",
+  appId: "1:833741969388:web:4bd71fc8c5281323f112c9",
+  measurementId: "G-72YFDZ7LJ6",
+};
+
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+export const auth = getAuth(app);
+export const googleAuthProvider = new GoogleAuthProvider();
+
+export const firestore = getFirestore(app);
+
+analytics.app.automaticDataCollectionEnabled = true;
