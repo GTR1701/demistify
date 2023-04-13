510664c63bbcab2a39fe6c700e785064f1eaf542
GTR1701 <dawidkowal04@gmail.com>
2023-04-13 13:09:56 +0200

    Functional navbar

--- client/components/Navbar.tsx
+++ client/components/Navbar.tsx
@@ -1,5 +1,4 @@
-//create a MUI responsive navbar in nextjs13 with typescript and mui
-import React from "react";
+//MUI responsive navbar in nextjs13 with typescript and mui
 import {
   AppBar,
   Toolbar,
@@ -10,26 +9,56 @@ import {
   useTheme,
   Drawer,
   List,
-  ListItem,
   ListItemText,
   ListItemIcon,
   Divider,
   Box,
   ListItemButton,
+  AccordionSummary,
+  AccordionDetails,
+  Switch,
+  styled,
+  FormControlLabel,
 } from "@mui/material";
 import MenuIcon from "@mui/icons-material/Menu";
+import HomeIcon from "@mui/icons-material/Home";
+import InfoIcon from "@mui/icons-material/Info";
+import LoginIcon from "@mui/icons-material/Login";
+import AlternateEmailIcon from "@mui/icons-material/AlternateEmail";
+import VpnKeyIcon from "@mui/icons-material/VpnKey";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
 import { useRouter } from "next/router";
 import { useState } from "react";
+import Accordion from "@mui/material/Accordion";
+import FormGroup from "@mui/material/FormGroup";
+
+type handleDrawer = {
+  handleDrawer: () => void;
+};
 
 export default function Navbar() {
   const [open, setOpen] = useState(false);
+  const [accordionOpen, setAccordionOpen] = useState(false);
   const router = useRouter();
   const theme = useTheme();
-  const isMobile = useMediaQuery(theme.breakpoints.down("md"));
+  const isMobile = useMediaQuery(theme.breakpoints.up("md"));
 
   function handleDrawer(): void {
     setOpen(!open);
   }
+
+  //implement a dark mode toggle
+  const [darkMode, setDarkMode] = useState(true);
+  const handleThemeChange = () => {
+    setDarkMode(!darkMode);
+    if (darkMode) {
+      theme.palette.mode = "dark";
+    } else {
+      theme.palette.mode = "light";
+    }
+  };
+
+  const label = { inputProps: { "aria-label": "Dark mode" } };
 
   return (
     <div>
@@ -45,24 +74,123 @@ export default function Navbar() {
           >
             <MenuIcon />
           </IconButton>
-          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            Nextjs13 MUI
-          </Typography>
-          <Button color="inherit" onClick={() => router.push("/")}>
-            Home
-          </Button>
-          <Button color="inherit" onClick={() => router.push("/about")}>
-            About
-          </Button>
-          <Button color="inherit" onClick={() => router.push("/contact")}>
-            Contact
-          </Button>
-          <Button color="inherit" onClick={() => router.push("/login")}>
-            Login
-          </Button>
-          <Button color="inherit" onClick={() => router.push("/register")}>
-            Register
-          </Button>
+          {isMobile ? (
+            <>
+              <Box sx={{ marginLeft: "1rem" }}>
+                <Button color="inherit" onClick={() => router.push("/")}>
+                  Home
+                </Button>
+                <Button color="inherit" onClick={() => router.push("/about")}>
+                  About
+                </Button>
+                <Button color="inherit" onClick={() => router.push("/contact")}>
+                  Contact
+                </Button>
+              </Box>
+              <Box sx={{ marginLeft: "auto", display: "flex" }}>
+                <FormGroup>
+                  <FormControlLabel
+                    control={
+                      <Switch
+                        {...label}
+                        onChange={() => handleThemeChange()}
+                        color={darkMode ? "default" : "secondary"}
+                      />
+                    }
+                    label={
+                      <Typography>
+                        {darkMode ? "Toggle light mode" : "Toggle dark mode"}
+                      </Typography>
+                    }
+                    labelPlacement="start"
+                  />
+                </FormGroup>
+                <Button
+                  color="inherit"
+                  sx={{ marginLeft: "auto" }}
+                  onClick={() => router.push("/login")}
+                >
+                  <LoginIcon />
+                  <Typography sx={{ marginLeft: "0.5rem" }}>Login</Typography>
+                </Button>
+              </Box>
+            </>
+          ) : (
+            //mui accordion menu
+            <>
+              <Accordion
+                sx={{
+                  backgroundColor: "inherit",
+                  width: "90%",
+                  border: "none",
+                  boxShadow: "none",
+                  color: "inherit",
+                }}
+                onClick={() => setAccordionOpen(!accordionOpen)}
+              >
+                <AccordionSummary
+                  aria-controls="panel1d-content"
+                  id="panel1d-header"
+                  sx={{ textAlign: "center", width: "100%" }}
+                >
+                  {!accordionOpen ? (
+                    <ExpandMoreIcon sx={{ margin: "auto" }} />
+                  ) : (
+                    <ExpandMoreIcon
+                      sx={{ margin: "auto", transform: "rotate(180deg)" }}
+                    />
+                  )}
+                </AccordionSummary>
+                <AccordionDetails>
+                  <Box sx={{ display: "flex", flexDirection: "column" }}>
+                    <FormGroup sx={{ margin: "auto" }}>
+                      <FormControlLabel
+                        control={
+                          <Switch
+                            {...label}
+                            onChange={() => handleThemeChange()}
+                            color={darkMode ? "default" : "secondary"}
+                          />
+                        }
+                        label={
+                          <Typography>
+                            {darkMode
+                              ? "Toggle light mode"
+                              : "Toggle dark mode"}
+                          </Typography>
+                        }
+                        labelPlacement="start"
+                      />
+                    </FormGroup>
+                    <Button color="inherit" onClick={() => router.push("/")}>
+                      Home
+                    </Button>
+                    <Button
+                      color="inherit"
+                      onClick={() => router.push("/about")}
+                    >
+                      About
+                    </Button>
+                    <Button
+                      color="inherit"
+                      onClick={() => router.push("/contact")}
+                    >
+                      Contact
+                    </Button>
+                    <Button
+                      color="inherit"
+                      onClick={() => router.push("/login")}
+                    >
+                      <LoginIcon />
+                      <Typography sx={{ marginLeft: "0.5rem" }}>
+                        Login
+                      </Typography>
+                    </Button>
+                  </Box>
+                </AccordionDetails>
+              </Accordion>
+            </>
+          )}
         </Toolbar>
       </AppBar>
       <Drawer anchor="left" open={open} onClose={handleDrawer}>
@@ -72,7 +200,7 @@ export default function Navbar() {
   );
 }
 
-function CustomDrawer({ handleDrawer }) {
+function CustomDrawer({ handleDrawer }: handleDrawer) {
   const router = useRouter();
   return (
     <Box
@@ -88,19 +216,19 @@ function CustomDrawer({ handleDrawer }) {
       <List>
         <ListItemButton key="Home" onClick={() => router.push("/")}>
           <ListItemIcon>
-            <img src="/home.svg" alt="home" />
+            <HomeIcon />
           </ListItemIcon>
           <ListItemText primary="Home" />
         </ListItemButton>
         <ListItemButton key="About" onClick={() => router.push("/about")}>
           <ListItemIcon>
-            <img src="/about.svg" alt="about" />
+            <InfoIcon />
           </ListItemIcon>
           <ListItemText primary="About" />
         </ListItemButton>
         <ListItemButton key="Contact" onClick={() => router.push("/contact")}>
           <ListItemIcon>
-            <img src="/contact.svg" alt="contact" />
+            <AlternateEmailIcon />
           </ListItemIcon>
           <ListItemText primary="Contact" />
         </ListItemButton>
@@ -109,15 +237,9 @@ function CustomDrawer({ handleDrawer }) {
       <List>
         <ListItemButton key="Login" onClick={() => router.push("/login")}>
           <ListItemIcon>
-            <img src="/login.svg" alt="login" />
+            <VpnKeyIcon />
           </ListItemIcon>
           <ListItemText primary="Login" />
-        </ListItemButton>
-        <ListItemButton key="Register" onClick={() => router.push("/register")}>
-          <ListItemIcon>
-            <img src="/register.svg" alt="register" />
-          </ListItemIcon>
-          <ListItemText primary="Register" />
         </ListItemButton>
       </List>
     </Box>
