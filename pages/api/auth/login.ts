d002ce8b602349ff76b167fdba4ac930782fb4f9
GTR1701 <dawidkowal04@gmail.com>
2023-05-31 16:18:20 +0200

    test

--- pages/api/auth/login.ts
+++ pages/api/auth/login.ts
@@ -8,32 +8,32 @@ export default async function handler(
 ) {
   const username = req.body.username;
   const password = req.body.password;
-  console.log(req.body);
-  const user = await prisma.users.findMany({
-    where: {
-      OR: [
-        {
-          username: username,
-        },
-        {
-          email: username,
-        },
-      ],
-    },
-  });
-  console.log(user);
-  if (!user) {
-    res.send("User not found");
-    return;
-  }
-  const valid = await bcrypt.compare(password, user[0].password);
-  if (!valid) {
-    res.send("Invalid password");
-    return;
-  }
-  const value = {
-    uid: user[0].uid,
-    username: user[0].username,
-  };
-  res.status(200).json(value);
+  console.log(username, password);
+  // const user = await prisma.users.findMany({
+  //   where: {
+  //     OR: [
+  //       {
+  //         username: username,
+  //       },
+  //       {
+  //         email: username,
+  //       },
+  //     ],
+  //   },
+  // });
+  // console.log(user);
+  // if (!user) {
+  //   res.send("User not found");
+  //   return;
+  // }
+  // const valid = await bcrypt.compare(password, user[0].password);
+  // if (!valid) {
+  //   res.send("Invalid password");
+  //   return;
+  // }
+  // const value = {
+  //   uid: user[0].uid,
+  //   username: user[0].username,
+  // };
+  // res.status(200).json(value);
 }
