3540057343f2a3a116ddd6acf74a5091256994a1
GTR1701 <dawidkowal04@gmail.com>
2023-06-18 10:26:36 +0200

    started work with monaco editor

--- components/SelectBlockLesson.tsx
+++ components/SelectBlockLesson.tsx
@@ -1,15 +1,25 @@
 import { Box, Typography } from "@mui/material";
+import Landing from "./Landing";
 
 export function getServerSideProps() {}
 
 export default function SelectBlockLesson() {
   return (
-    <Box sx={{ display: "flex", width: "100%", height: "95vh" }}>
-      <Box sx={{ width: "50%", height: "100%", backgroundColor: "purple" }}>
+    <Box sx={{ display: "flex", width: "100%", height: "93vh" }}>
+      <Box
+        sx={{
+          width: "25%",
+          height: "100%",
+          backgroundColor: "purple",
+          overflowY: "scroll",
+          scrollbarWidth: "none",
+          scrollbarColor: "transparent transparent",
+        }}
+      >
         <Typography>Box 1</Typography>
       </Box>
-      <Box sx={{ width: "50%", height: "100%", backgroundColor: "blue" }}>
-        <Typography>Box 2</Typography>
+      <Box sx={{ width: "75%", height: "100%", backgroundColor: "blue" }}>
+        <Landing />
       </Box>
     </Box>
   );
