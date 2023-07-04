04656926ee555dac11bb3e1ddd89c11f1519acf2
GTR1701 <dawidkowal04@gmail.com>
2023-07-04 11:32:43 +0200

    potential hotfix to rerun the site

--- pages/api/lessons/data.ts
+++ pages/api/lessons/data.ts
@@ -5,11 +5,10 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  const data = req.body;
-  console.log(data);
-  const value = await prisma.lessons.findUnique({
-    where: {},
-  });
-
-  res.status(200).send(value);
+  // const data = req.body;
+  // console.log(data);
+  // const value = await prisma.lessons.findUnique({
+  //   where: {},
+  // });
+  // res.status(200).send(value);
 }
